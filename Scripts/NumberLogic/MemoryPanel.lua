774b636f93ea7ec2086889e6b96994ca921068e5
brentbatch <brent.batch@hotmail.com>
2019-06-21 18:50:27 +0200

    removed jank from memorypanel & improved it

--- Scripts/NumberLogic/MemoryPanel.lua
+++ Scripts/NumberLogic/MemoryPanel.lua
@@ -7,6 +7,7 @@ end
 dofile "../Libs/GameImprovements/interactable.lua"
 dofile "../Libs/MoreMath.lua"
 
+
 mpPrint("loading MemoryPanel.lua")
 
 -- MemoryPanel.lua --
@@ -19,117 +20,154 @@ MemoryPanel.colorNormal = sm.color.new( 0x7F567Dff )
 MemoryPanel.colorHighlight = sm.color.new( 0x9f7fa5ff )
 MemoryPanel.poseWeightCount = 1
 
+MemoryPanel.data = {[0] = 0}
 
 function MemoryPanel.server_onRefresh( self )
 	sm.isDev = true
 	self:server_onCreate()
 end
 
-function MemoryPanel.server_onCreate( self ) 
-	self.data = {}
-	self.data[0] = 0
-	self.data["key"] = 1
-	--self.data[1] = 1
-	self.power = 0
+function MemoryPanel.server_onCreate( self )
+	sm.ImproveUserData(self)
 	self.mode = 0
 	self.time = 0
-	local stored = self.storage:load() 
-	--print(stored)
+	local value = 0
+	local stored = self.storage:load()
 	if stored then
-		if type(stored) == "number" then 
+		if type(stored) == "number" then --very old compatibility support
 			self.data[0] = stored
 		elseif type(stored) == "table" then
 			self.data = stored
 		end
-		self.power = tonumber(self.data[0])
-		if not self.power then self.power = 0 end
-		self.interactable:setPower(self.power)
+		value = tonumber(self.data[0]) or 0
 	else
 		self.storage:save(self.data)
 	end
-	sm.interactable.setValue(self.interactable, self.power)
+	self.interactable:setValue(value)
+	if value ~= value then value = 0 end
+	if math.abs(value) >= 3.3*10^38 then 
+		if value < 0 then value = -3.3*10^38 else value = 3.3*10^38 end  
+	end
+	self.interactable:setPower(value)
 end
 
 
 
 function MemoryPanel.server_onFixedUpdate( self, dt )
 	local parents = self.interactable:getParents()
-	local saves = false
 	local address = 0
 	local value = 0
 	local writevalue = false
 	local hasvalueparent = false
 	local reset = false
 	for k,v in pairs(parents) do
-		if v:getType() == "scripted" and tostring(v:getShape():getShapeUuid()) ~= "6f2dd83e-bc0d-43f3-8ba5-d5209eb03d07" then
+		if v:isNumberType() then
 			-- number input
-			if tostring(sm.shape.getColor(v:getShape())) == "eeeeeeff" then
+			if tostring(v:getShape().color) == "eeeeeeff" then
 				-- address
-				address = address + (sm.interactable.getValue(v) or v.power)
+				address = address + (v:getValue() or v.power)
 			else
 				-- value
-				value = value + (sm.interactable.getValue(v) or v.power)
+				value = value + (v:getValue() or v.power)
 				hasvalueparent = true
 			end
 		else
 			-- logic input
-			if (sm.interactable.getValue(v) or v.power) > 0 then writevalue = true end
-			if tostring(sm.shape.getColor(v:getShape())) == "222222ff" and (sm.interactable.getValue(v) or v.power) ~= 0 then
-				reset = true
+			if v.active then 
+				writevalue = true
+				if tostring(v:getShape().color) == "222222ff" then
+					reset = true
+				end
 			end
 		end
 	end
+	
+	local saves = false
 	if writevalue and hasvalueparent then
-		saves = self.data[address] ~= value
 		self.data[address] = tostring(value)
+		saves = self.data[address] ~= value
 	end
-	self.power = tonumber(self.data[address])
+	local power = tonumber(self.data[address]) or 0 
 	if reset then
-		self.power = 0
-		self.data = {}
-		self.data[0] = 0
-		self.data["key"] = 1
+		power = 0
+		self.data = {[0] = 0}
+		saves = true
+	end
+	
+	if saves then
+		self.storage:save(self.data)
+	end
+	
+	if power ~= (self.interactable:getValue() or self.interactable.power) then
+		print('power changed')
+		self.interactable:setValue(power)
+		
+		if power ~= power then power = 0 end
+		if math.abs(power) >= 3.3*10^38 then 
+			if power < 0 then power = -3.3*10^38 else power = 3.3*10^38 end  
+		end
+		self.interactable:setActive(power>0)
+		self.interactable:setPower(power)
+	end
+end
+
+function MemoryPanel.client_onFixedUpdate(self, dt)
+	local parents = self.interactable:getParents()
+	local address = 0
+	local writevalue = false
+	local reset = false
+	for k,v in pairs(parents) do
+		if v:isNumberType() then
+			-- number input
+			if tostring(v:getShape().color) == "eeeeeeff" then
+				-- address
+				address = address + (v:getValue() or v.power)
+			else
+				hasvalueparent = true
+			end
+		else
+			-- logic input
+			if v.active then 
+				writevalue = true
+				if tostring(v:getShape().color) == "222222ff" then
+					reset = true
+				end
+			end
+		end
+	end
+	
+	if writevalue and hasvalueparent then
+		self.time = 20
+		self.mode = 1
+	end
+	
+	if reset then
 		self.time = 60
 		self.mode = 1
-		saves = false
-		self.storage:save(self.data)
-		address = 0
 	end
-	if self.power == nil then self.power = 0 end
-	if self.power ~= self.interactable.power or self.power ~= (sm.interactable.getValue(self.interactable) or self.interactable.power) then
+	
+	if self.lastPower ~= self.interactable.power and self.mode == 0 then
 		self.time = 20
 		self.mode = 2
 	end
-	if self.interactable.power ~= self.power and type(self.power) == "number" and math.abs(self.power) >= 0 then
-		self.interactable:setActive(self.power>0)
-		self.interactable:setPower(self.power)
-		sm.interactable.setValue(self.interactable, self.power)
-	end
-	if saves then 
-		self.time = 20
-		self.mode = 1
-		self.data["key"] = 1
-		self.storage:save(self.data)
-	end
-	if address ~= self.lastaddress then self.network:sendToClients("client_setUvValue", address) end
+	
+	
+	if address ~= self.lastaddress then self:client_setUvValue(address) end
+	
 	if self.mode == 1 then
-		self.network:sendToClients("client_setPose", (self.time%4)>2 and 1 or 0)
+		self.interactable:setPoseWeight(0,(self.time%4)>2 and 1 or 0)
 	elseif self.mode == 2 then	
-		self.network:sendToClients("client_setPose", self.time>0 and 1 or 0)
+		self.interactable:setPoseWeight(0, self.time>0 and 1 or 0)
 	else
-		self.network:sendToClients("client_setPose", 0)
+		self.interactable:setPoseWeight(0, 0)
 	end
 	self.time = self.time > 0 and self.time - 1 or 0
 	self.mode = self.time > 0 and self.mode or 0
+	
+	self.lastPower = self.interactable.power
 	self.lastaddress = address
 end
 
-function MemoryPanel.client_setUvframeIndex(self, index)
-	self.interactable:setUvFrameIndex(index)
-end
-function MemoryPanel.client_setPose(self, level)
-	self.interactable:setPoseWeight(0, level)
-end
 function MemoryPanel.client_setUvValue(self, value) 
 	if value < 0 then 
 		value = 0-value 
