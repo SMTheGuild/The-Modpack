cecedab44d3aa85671a93524ad6123cc6ea9d472
Xesau <aron2000@zeelandnet.nl>
2021-04-12 11:05:28 +0200

    Clean up Decoupler code

--- Scripts/interactable/Decoupler.lua
+++ Scripts/interactable/Decoupler.lua
@@ -1,21 +1,14 @@
---[[
-	Copyright (c) 2020 Modpack Team
-	Brent Batch#9261 for copy pasta
-]]--
-dofile "../libs/load_libs.lua"
-
-mpPrint("loading Decoupler.lua")
-
--- Decoupler.lua --
-Decoupler = class( nil )
+Decoupler = class(nil)
 Decoupler.maxChildCount = 0
 Decoupler.maxParentCount = 1
 Decoupler.connectionInput = sm.interactable.connectionType.logic
 Decoupler.connectionOutput = sm.interactable.connectionType.none
-Decoupler.colorNormal = sm.color.new( 0x844040ff )
-Decoupler.colorHighlight = sm.color.new( 0xb25959ff )
+Decoupler.colorNormal = sm.color.new(0x844040ff)
+Decoupler.colorHighlight = sm.color.new(0xb25959ff)
 
 function Decoupler.server_onFixedUpdate(self, dt)
 	local parent = self.interactable:getSingleParent()
-	if parent and parent:isActive() then sm.shape.destroyPart(self.shape) end
+	if parent and parent:isActive() then
+        sm.shape.destroyPart(self.shape)
+    end
 end
