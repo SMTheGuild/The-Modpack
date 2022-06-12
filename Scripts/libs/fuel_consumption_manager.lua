8606ef6cda227f4855c49e9f1cfc34a4e7219df2
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-06-12 11:13:14 -0700

    SmartController can consume electricity now

--- Scripts/libs/fuel_consumption_manager.lua
+++ Scripts/libs/fuel_consumption_manager.lua
@@ -2,10 +2,11 @@ dofile("$SURVIVAL_DATA/Scripts/game/survival_items.lua") --to get the uuids of c
 
 local _sm_getEnableFuelConsumption = sm.game.getEnableFuelConsumption
 
-function mp_fuel_initialize(self, fuel_uuid, fuel_multiplier)
+function mp_fuel_initialize(self, fuel_uuid, fuel_multiplier, connection_type)
 	self.sv_fuel_points = 0
 	self.sv_fuel_multiplier = fuel_multiplier
 	self.sv_fuel_uuid = fuel_uuid
+	self.sv_fuel_connect_type = connection_type or sm.interactable.connectionType.gasoline
 end
 
 function mp_fuel_consumeFuelPoints(self, container, power, dt)
@@ -22,7 +23,7 @@ function mp_fuel_consumeFuelPoints(self, container, power, dt)
 end
 
 function mp_fuel_getValidFuelContainer(self)
-	local parents = self.interactable:getParents(sm.interactable.connectionType.gasoline)
+	local parents = self.interactable:getParents(self.sv_fuel_connect_type)
 	for k, v in pairs(parents) do
 		local gas_container = v:getContainer()
 
@@ -45,13 +46,13 @@ function mp_fuel_canConsumeFuel(self, container)
 	return is_valid_active, not useCreativeFuel
 end
 
-function mp_fuel_displayOutOfFuelMessage(self)
+function mp_fuel_displayOutOfFuelMessage(self, custom_message)
 	local l_player = sm.localPlayer.getPlayer()
 	local l_character = l_player:getCharacter()
 
 	if l_character then
 		if (self.shape.worldPosition - l_character.worldPosition):length2() < 100 then
-			sm.gui.displayAlertText("#{INFO_OUT_OF_FUEL}")
+			sm.gui.displayAlertText(custom_message or "#{INFO_OUT_OF_FUEL}")
 		end
 	end
 end
